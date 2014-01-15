3ffc0acd3ce9e29646fc13e6c2b38fa842c28ca7
agnikai <jacquelyn905@gmail.com>
2014-01-15 15:04:28 -0500

    Update slide.rb

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,8 +1,8 @@
 class Spree::Slide < ActiveRecord::Base
 
   has_attached_file :image
-  include Spree::Core::S3Support
-  supports_s3 :image
+  # include Spree::Core::S3Support
+  # supports_s3 :image
 
   scope :published, -> { where(published: true).order('position ASC') }
 
